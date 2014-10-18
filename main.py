7156183544b39580012da89b57bf782334337101
eightnoteight <mr.eightnoteight@gmail.com>
2014-10-18 09:14:17 +0530

    datastore api

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import time
+from flask import redirect, url_for, request
+from google.appengine.ext import db
+from google.appengine.api import users
+
+
+class NonsensoUsers(db.Model):
+    email = db.StringProperty(required=True)
+    epoch = db.IntegerProperty(required=True)
+    latest = db.IntegerProperty()
+    school = db.StringProperty(required=True)
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -7,12 +19,35 @@ app.config['DEBUG'] = True
 
 
 @app.route('/')
-def hello():
+def index():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
 
+@app.login('/login')
+def login():
+    user = users.get_current_user()
+    if user:
+        return app.send_static_file('successfully_logged_in.html')
+    return app.send_static_file('login.html')
+
+
+@app.route('/quiz')
+def quiz():
+    user = users.get_current_user()
+    if not user:
+        return redirect(users.create_login_url(request.base_url))
+    return 'entered into the quiz'
+
+
+# 4xx error handlers
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+    return '<h1>Sorry, nothing at this URL.<h1>', 404
+
+
+@app.errorhandler(401)
+def unauthorized(e):
+    """ custom 401 error"""
+    return '<h1>hey, you there, no wandering around here</h1>', 401
